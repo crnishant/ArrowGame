d425ddd3336219f495564cdc7726036e60cba7dc
Nishant Chintala <crnishant@gmail.com>
2020-10-01 10:45:04 -0400

    scores progress

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
-let $playerInput = document.getElementById("playerAnswer");
-var arrows = ["bluedownarrow", "blueleftarrow", "bluerightarrow", "blueuparrow",
+var playerInput = document.getElementById("playerAnswer");
+var score = 0;
+var gameState = "active";
+var scoreInc = false;
+let arrows = ["bluedownarrow", "blueleftarrow", "bluerightarrow", "blueuparrow",
 "reddownarrow", "redleftarrow", "redrightarrow", "reduparrow"];
+let currentImage = arrows[0];
 
-document.getElementById("go").addEventListener("click", function(){
-    document.getElementById("gameboard").innerHTML = "";
-    var img = document.createElement("img");
-    img.src = "/images/"+arrows[Math.floor(Math.random()*8)] + ".png";
-    var src = document.getElementById("gameboard");
-    src.appendChild(img);
-})
+function updateGame(){
+	document.getElementById("gameboard").innerHTML = "";
+	setTimeout(createNewImage, 500);
+	document.addEventListener("keyup", event => {
+		if(event.keyCode== 37 && scoreInc){
+			//console.log("left");
+			if(currentImage=="blueleftarrow" || currentImage == "redrightarrow"){
+				score+=100;
+			}
+			scoreInc = false;
+			console.log(score);
+		}
+		else if(event.which == 39 && scoreInc){
+			//console.log("right");
+			if(currentImage=="bluerightarrow" || currentImage == "redleftarrow"){
+				score+=100;
+			}
+			scoreInc = false;
+			console.log(score);
+		}
+		else if(event.which == 38 && scoreInc){
+			//console.log("up");
+			if(currentImage=="blueuparrow" || currentImage == "reddownarrow"){
+				score+=100;
+			}
+			scoreInc = false;
+			console.log(score);
+		}
+		else if(event.which == 40 && scoreInc){
+			//console.log("down");
+			if(currentImage=="bluedownarrow" || currentImage == "reduparrow"){
+				score+=100;
+			}
+			scoreInc = false;
+			console.log(score);
+		}
+	});
+}
+
+function createNewImage(){
+	var img = document.createElement("img");
+	currentImage = arrows[Math.floor(Math.random()*8)];
+	img.src = "/images/"+ currentImage + ".png";
+	var src = document.getElementById("gameboard");
+	src.appendChild(img);
+	scoreInc = true;
+}
+
+function start(){
+	var self = this;
+	setInterval(updateGame,1000);
+}
+
+start();
+
